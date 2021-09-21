65a95d2c91e209c9786ef00b1df3955f4a351a22
Zymass <filyaevilya21@gmail.com>
2021-09-21 18:07:56 +0300

    Initial Commit

---
+++ SendMessage/SendMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendMessageApp.swift
+//  SendMessage
+//
+//  Created by Feduk on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SendMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
